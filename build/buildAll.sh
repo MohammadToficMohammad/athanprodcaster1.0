ace7e3f016304dba5ab391c02fa73396f406b1e1
MohammadToficMohammad <mohammad.tofic.mohammad@gmail.com>
2020-12-25 00:16:08 +0100

    init

--- build/buildAll.sh
+++ build/buildAll.sh
@@ -1,26 +1,26 @@
 cd ..
 cd LogglyEvents
-mvn clean install -DskipTests
+mvn clean install -DskipTests -Dmaven.test.skip=true 
 cd ..
 cd ServiceRegistery
-mvn clean install -DskipTests
+mvn clean install -DskipTests -Dmaven.test.skip=true 
 cd ..
 cd ConfigServer
-mvn clean install -DskipTests
+mvn clean install -DskipTests -Dmaven.test.skip=true 
 cd ..
 cd AuthorizationServiceRpcClient
-mvn clean install -DskipTests
+mvn clean install -DskipTests -Dmaven.test.skip=true 
 cd ..
 cd MqClientAbstracts
-mvn clean install -DskipTests
+mvn clean install -DskipTests -Dmaven.test.skip=true 
 cd ..
 cd MqServerAbstracts
-mvn clean install -DskipTests
+mvn clean install -DskipTests -Dmaven.test.skip=true 
 cd ..
 cd AuthorizationService
-mvn clean install -DskipTests
+mvn clean install -DskipTests -Dmaven.test.skip=true 
 cd ..
 cd ProjectService
-mvn clean install -DskipTests
+mvn clean install -DskipTests -Dmaven.test.skip=true 
 cd ..
 cd build

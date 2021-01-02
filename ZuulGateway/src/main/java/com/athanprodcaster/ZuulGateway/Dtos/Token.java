cc3aed34d8f712c1731d9ab18ffe57c85b94e97c
MohammadToficMohammad <mohammad.tofic.mohammad@gmail.com>
2021-01-02 18:50:28 +0100

    jwt rsa

--- ZuulGateway/src/main/java/com/athanprodcaster/ZuulGateway/Dtos/Token.java
+++ ZuulGateway/src/main/java/com/athanprodcaster/ZuulGateway/Dtos/Token.java
@@ -2,6 +2,12 @@ package com.athanprodcaster.ZuulGateway.Dtos;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.RSAPublicKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.sql.Timestamp;
 import java.util.Base64;
 
@@ -13,7 +19,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
 
+import java.math.BigInteger;
 import java.security.Key;
+import java.security.KeyFactory;
+import java.security.KeyPair;
 
 import io.jsonwebtoken.*;
 
@@ -115,8 +124,65 @@ public class Token {
 	            .setSubject("AthanProdcaster")
 	           // .setPayload(payloadjson)
 	            .setIssuer("AthanProdcaster")
-	            .addClaims(payloadClaim)
-	            .signWith(signatureAlgorithm, signingKey);
+	            .addClaims(payloadClaim).signWith(signingKey);
+	           // .signWith(signatureAlgorithm, signingKey);
+	  
+	    //if it has been specified, let's add the expiration
+	    if (expireTimestampMillis > 0) {
+	        long expMillis = nowMillis + expireTimestampMillis;
+	        Date exp = new Date(expMillis);
+	        builder.setExpiration(exp);
+	    }  
+	  
+	    //Builds the JWT and serializes it to a compact, URL-safe string
+	    return builder.compact();
+	}
+	
+	public static String createRsaJWT(long expireTimestampMillis, LoginResultDto payload,String secret)  {
+		  
+		
+		
+	    //The JWT signature algorithm we will be using to sign the token
+	    SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.RS256;
+
+	    long nowMillis = System.currentTimeMillis();
+	    Date now = new Date(nowMillis);
+
+	    //We will sign our JWT with our ApiKey secret
+	    byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(secret);
+	   // byte[] apiKeySecretBytes  =Base64.getEncoder().encode(secret.getBytes());
+	    //Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+	
+	    
+	    byte [] pkcs8EncodedBytes = Base64.getDecoder().decode(secret);
+
+        // extract the private key
+
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(pkcs8EncodedBytes);
+        KeyFactory kf;
+        PrivateKey privKey =null;
+		try {
+			kf = KeyFactory.getInstance("RSA");
+		    privKey = kf.generatePrivate(keySpec);
+		} catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+       
+       
+	    
+	    
+	    //Let's set the JWT Claims
+	    //JwtBuilder builder = Jwts.builder().setId(id)
+	    var payloadClaim=new HashMap<String,Object>();
+	    payloadClaim.put("payload", payload);
+	    JwtBuilder builder = Jwts.builder()
+	            .setIssuedAt(now)
+	            .setSubject("AthanProdcaster")
+	           // .setPayload(payloadjson)
+	            .setIssuer("AthanProdcaster")
+	            .addClaims(payloadClaim).signWith(privKey);
+	           // .signWith(signatureAlgorithm, signingKey);
 	  
 	    //if it has been specified, let's add the expiration
 	    if (expireTimestampMillis > 0) {
@@ -130,12 +196,52 @@ public class Token {
 	}
 	
 	
+	public static LoginResultDto decodeRsaJWT(String jwt,String secret) {
+	    //This line will throw an exception if it is not a signed JWS (as expected)
+		
+		
+		byte[] decoded = Base64.getDecoder().decode(secret);
+		//org.bouncycastle.asn1.pkcs.RSAPublicKey pkcs1PublicKey = org.bouncycastle.asn1.pkcs.RSAPublicKey.getInstance(decoded);
+		//BigInteger modulus = pkcs1PublicKey.getModulus();
+		//BigInteger publicExponent = pkcs1PublicKey.getPublicExponent();
+		//RSAPublicKeySpec keySpec = new RSAPublicKeySpec(modulus, publicExponent);
+		KeyFactory kf;
+		PublicKey generatedPublic =null;
+		try {
+			kf = KeyFactory.getInstance("RSA");
+		    //generatedPublic = kf.generatePublic(keySpec);
+		    generatedPublic = kf.generatePublic(new X509EncodedKeySpec(decoded));
+		} catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println( Jwts.parserBuilder().setSigningKey(generatedPublic).build().isSigned(jwt));
+		
+
+		  
+	    Claims claims = (Claims) Jwts.parserBuilder().setSigningKey(generatedPublic).build().parse(jwt).getBody();
+	            
+		ObjectMapper mapper = new ObjectMapper();
+
+		LoginResultDto loginResultDto = mapper.convertValue(claims.get("payload"), LoginResultDto.class);
+
+	    return loginResultDto;
+	}
+	
+	
 	public static LoginResultDto decodeJWT(String jwt,String secret) {
 	    //This line will throw an exception if it is not a signed JWS (as expected)
 		
-	    Claims claims = Jwts.parser()
-	            .setSigningKey(DatatypeConverter.parseBase64Binary(secret))
-	            .parseClaimsJws(jwt).getBody();
+		 SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+
+
+		    //We will sign our JWT with our ApiKey secret
+		    byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(secret);
+		   // byte[] apiKeySecretBytes  =Base64.getEncoder().encode(secret.getBytes());
+		    Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+		
+	    Claims claims = (Claims) Jwts.parserBuilder().setSigningKey(signingKey).build().parse(jwt).getBody();
 	            
 		ObjectMapper mapper = new ObjectMapper();
 
--- ZuulGateway/src/main/java/com/athanprodcaster/ZuulGateway/SecretContainer.java
+++ ZuulGateway/src/main/java/com/athanprodcaster/ZuulGateway/SecretContainer.java
@@ -7,7 +7,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SecretContainer {
-	
+	//for jwt debugger add this -----END PUBLIC KEY----- 
+	public static String rsaPublicKey="MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAugVQkLggzy8IeKXD6xIpRkzfn1w5e1rjw+5+6RoT1WQBvs1455lsp4rfKcJzF2fwQ23darDrvgSS3hnI4EVK/RBu2KWEo1ghS5Qzu/pLV1lY/doi0Wy3U9VZ8JH6pWVqWpsKzsYY8cRXo0ld82JUP64AmIj+5Z/O4eZCIE+hILQ3P80HTlqdIYGBmt79mUiCqhs1f6aRvh9XagydjQWJ7QHYwfDO6v6qwGwBgDiS68IJldFzt2BjkItP9GFIbC7BWvSpOe0ttSGZmB61CnTnGYf9A0a1kk72BoY37lnprrVZ+1Bgd/etxqo9NWBMAnqT7Yop6TEnbVWC+Rhl2edGWQIDAQAB";
+	public static String rsaPrivateKey="MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC6BVCQuCDPLwh4pcPrEilGTN+fXDl7WuPD7n7pGhPVZAG+zXjnmWynit8pwnMXZ/BDbd1qsOu+BJLeGcjgRUr9EG7YpYSjWCFLlDO7+ktXWVj92iLRbLdT1VnwkfqlZWpamwrOxhjxxFejSV3zYlQ/rgCYiP7ln87h5kIgT6EgtDc/zQdOWp0hgYGa3v2ZSIKqGzV/ppG+H1dqDJ2NBYntAdjB8M7q/qrAbAGAOJLrwgmV0XO3YGOQi0/0YUhsLsFa9Kk57S21IZmYHrUKdOcZh/0DRrWSTvYGhjfuWemutVn7UGB3963Gqj01YEwCepPtiinpMSdtVYL5GGXZ50ZZAgMBAAECggEBAIYuGTwapQftjDXClHy2v/EGuHNyCcCSAiMtoHuyJMZS+fNQsTqV78s2n9y2d2BVx/hLgmBYN+P71pjAjqY/NWUvelVppzBP/MxQnbSlSb07JLcPpvfbbK3bAqJGGj891SaAGZ6nRAz4rKKK5GsP7Wd1stZUU1D9IjTOa7p01ewI99Mf17XwJIaoLI/Tp6YDmajHe5VtLry6PqT3pfgO2txPNCsJPv2OHVYwA0I3AcmVK0KFRa+TX+7TCcPAehK3cNiOBZOsQJUo9vNPX19yJt5FO07t504XU64sr8iHzgQhJCDj8dv3KrE9osBieJGT2/Latg2O0tjm5qRqoOxMi5ECgYEA2mAym7zSJ9HRqm5amPQYP2YD7PUUIbFe1OBNDZqoSoUtW3gNBzmlf4BM1Ku69cX71VTUXvx3J7CvHp/q7tnSzv7fyySriZjgO9hXF4sjDNjVxF2xiMA0AEJkd+GjGXJgJDaVaRh7x24T9Fz6OuJTun/eC4I2yQpdQh71XwuOKaUCgYEA2hIMjLYt9bMWWhs2xFNViHHvwpkzPYUG70AJ0AMPjHLs/cWiPkjf757Rc5i93JGovXK3BBmzenQE2RUcuKiq6m/i3IZqo6oBMZR813xl5KBR5/UJHzemjYX2KuulEHkTdfkFRY9yl7fj3Ao3/61T5DIYLsFSsN/vkGxvwUBrg6UCfx+Px8C6iaU3rhXz3SQ+I0MGe4rD4ST9fbdGs9xDfg9HMEcIEtn4evLymCgkqeDbvIoYuJkB5DbYUncapdnfoZhJPg8ePayla8M6gVvlwCsbK8o1wHMDVIC1F9A7zFbhMQQK341Q/JLWVgv7ZC8ehvttf6oqJ8DmvpPP5abXBLUCgYEAuFmnyFC+m8Yw8C2vZ9r3JOkFAmKW4bwSt7FQWkwsGdSSKpqn5LWgsiZ19J48azmsB7PxqgK8Kpnn+0iQxj64doXtp/XuMrumVK6tt1FPpi4t78TDWGOoZl3IVFJvcXSZkhLZdQummUH42+y7mGaD1BbRQL1n1WUKVfKFqz51ZY0CgYEA1XVpqEIy5f2+zC9d7oYv6Ib2CaLFn6dBvD1fhUsU6KJmhbi+EhlDafvfZXXInwcPOiVpPzg0lovqFn74AfcxGbJ1cgOM36TgvUI3Fu0eFpmL2iscEAcsID3IWrkidnfJzGv6fZX1C6kqGD3i5EnmN3E3hw0/NODaifhvFn/qizI=";
+			
 	public static String secret= getSaltString();
 	public static long expireTimestamp= System.currentTimeMillis()+(1000 * 60 * 60 * 24);
 	@Scheduled(cron ="0 0 4 * * *")
@@ -20,7 +23,7 @@ public class SecretContainer {
         String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
         StringBuilder salt = new StringBuilder();
         Random rnd = new Random();
-        while (salt.length() < 18) { // length of the random string.
+        while (salt.length() < 64) { // length of the random string.
             int index = (int) (rnd.nextFloat() * SALTCHARS.length());
             salt.append(SALTCHARS.charAt(index));
         }
